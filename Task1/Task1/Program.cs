acf41ce6d5feca95ca5bcd1d6dc08eecb889de3c
mishka49 <61920146+mishka49@users.noreply.github.com>
2020-11-02 12:16:12 +0300

    Task1-Переделал

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -4,29 +4,6 @@ namespace Task1
 {
     class Program
     {
-<<<<<<< Updated upstream
-        static bool CheckingInput(out double a, out double b, out double c)
-        {
-            Console.WriteLine("Enter the values of the sides of the triangle");
-            try
-            {
-                a = double.Parse(Console.ReadLine());
-                b = double.Parse(Console.ReadLine());
-                c = double.Parse(Console.ReadLine());
-            }
-            catch
-            {
-                Console.WriteLine("Error");
-                return CheckingInput(out a, out b, out c);
-            }
-            return true;
-
-        }
-        static bool CheckingExistence(double a, double b, double c)
-        {
-            if (a + b > c && a+c>b && b+c>a)
-                return true;
-=======
         static void CheckInput(out double a)
         {
             string s;
@@ -51,38 +28,22 @@ namespace Task1
             {
                 return true;
             }
->>>>>>> Stashed changes
             else
             {
                 Console.WriteLine("This triangle cannot exist");
                 return false;
-<<<<<<< Updated upstream
-            }
-               
-        }
-        static void DefiningType(double a, double b, double c)
-=======
             }        
         }
         static void DetermineType(double a, double b, double c)
->>>>>>> Stashed changes
         {
             if (a == b && b == c)
             {
                 Console.WriteLine("Equilateral triangle");
             }
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
             else if (a == b || b == c || a == c)
             {
                 Console.WriteLine("Isosceles triangle");
             }
-<<<<<<< Updated upstream
-
-=======
->>>>>>> Stashed changes
             else
             {
                 Console.WriteLine("Versatile triangle");
@@ -90,13 +51,6 @@ namespace Task1
         }
         static void Main(string[] args)
         {
-<<<<<<< Updated upstream
-            double a, b, c;
-            CheckingInput(out a, out b, out c);
-            if (CheckingExistence(a, b, c))
-            {
-                DefiningType(a, b, c);
-=======
             Console.WriteLine("enter the first side");
             CheckInput(out double FirstSide);
             Console.WriteLine("Enter the second side");
@@ -106,7 +60,6 @@ namespace Task1
             if (CheckExistence(FirstSide, SecondSide, ThirdSide))
             {
                 DetermineType(FirstSide, SecondSide, ThirdSide);
->>>>>>> Stashed changes
             } 
             Console.ReadKey();
         }
