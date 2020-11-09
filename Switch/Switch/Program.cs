b1b857463e3d89a1f38bb2c3c60f6c41fc889835
mishka49 <61920146+mishka49@users.noreply.github.com>
2020-11-09 22:39:50 +0300

    Switch

---
+++ Switch/Switch/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Switch
+{
+    class Program
+    {
+        static string DefinesTypeOfVariable(object argument)
+        {
+            switch (argument.GetType().ToString())
+            {
+                case "System.Int32":
+                    return $"{argument} is int";
+                    break;
+                case "System.Double":
+                    return $"{argument} is double";
+                    break;
+                case "System.String":
+
+                    if (argument == "")
+                    {
+                        return "An empty value was entered";
+                    }
+                    else
+                    {
+                        return $"{argument} is string";
+                    }
+
+                    break;
+                case "System.Boolean":
+                    return $"{argument} is bool";
+                    break;
+                default:
+                    return "Type not defined";
+                    break;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
