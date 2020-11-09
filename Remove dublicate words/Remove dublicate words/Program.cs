749fc221ea1388804ef8cdb3605ac740f4373f87
mishka49 <61920146+mishka49@users.noreply.github.com>
2020-11-09 21:39:41 +0300

    RemoveDublicateWords

---
+++ Remove dublicate words/Remove dublicate words/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Remove_dublicate_words
+{
+    class Program
+    {
+        static string RemoveDublicateWords(string text)
+        {
+            string[] array = text.Split('.', ',', '!', '?', '-', ':', ';', ' ');
+
+            for (int j = 0; j < array.Length; j++)
+                for (int i = j + 1; i < array.Length; i++)
+                {
+                    if (array[j] == array[i])
+                    {
+                        array[i] = null;
+                    }
+                }
+
+            text = null;
+
+            foreach (string item in array)
+            {
+                text += item + " ";
+            }
+
+            return text;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input text");
+            string text = RemoveDublicateWords(Console.ReadLine());
+            Console.WriteLine("Text without dublicate: " + text);
+            Console.ReadKey();
+        }
+    }
+}
